e91c5312d5459fbcf87f663d26e92f7d065b7d2a
SHIVANSH475 <158449266+SHIVANSH475@users.noreply.github.com>
2024-02-20 17:14:49 +0530

    Update __init__.py

--- MukeshRobot/__init__.py
+++ MukeshRobot/__init__.py
@@ -52,7 +52,7 @@ if ENV:
         "START_IMG", ""
     )
     STRICT_GBAN = bool(os.environ.get("STRICT_GBAN", True))
-    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "the_friendz")
+    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "MASTIWITHFRIENDSX")
     TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./")
     TOKEN = os.environ.get("TOKEN", None)
     TIME_API_KEY = os.environ.get("TIME_API_KEY", None)
@@ -71,23 +71,23 @@ if ENV:
         raise Exception("Your blacklisted chats list does not contain valid integers.")
 
     try:
-        DRAGONS = set(int(x) for x in os.environ.get("DRAGONS", "6195725562").split())
-        DEV_USERS = set(int(x) for x in os.environ.get("DEV_USERS", "6195725562").split())
+        DRAGONS = set(int(x) for x in os.environ.get("DRAGONS", "6762113050").split())
+        DEV_USERS = set(int(x) for x in os.environ.get("DEV_USERS", "6762113050").split())
     except ValueError:
         raise Exception("Your sudo or dev users list does not contain valid integers.")
 
     try:
-        DEMONS = set(int(x) for x in os.environ.get("DEMONS", "6195725562").split())
+        DEMONS = set(int(x) for x in os.environ.get("DEMONS", "6762113050").split())
     except ValueError:
         raise Exception("Your support users list does not contain valid integers.")
 
     try:
-        TIGERS = set(int(x) for x in os.environ.get("TIGERS", "6195725562").split())
+        TIGERS = set(int(x) for x in os.environ.get("TIGERS", "6762113050").split())
     except ValueError:
         raise Exception("Your tiger users list does not contain valid integers.")
 
     try:
-        WOLVES = set(int(x) for x in os.environ.get("WOLVES", "6195725562").split())
+        WOLVES = set(int(x) for x in os.environ.get("WOLVES", "6762113050").split())
     except ValueError:
         raise Exception("Your whitelisted users list does not contain valid integers.")
 
