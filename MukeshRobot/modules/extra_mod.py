7aeef65a36ff803bf01067e81fd878f0d024fc52
SHIVANSH475 <158449266+SHIVANSH475@users.noreply.github.com>
2024-02-27 02:04:55 +0530

    Update extra_mod.py

--- MukeshRobot/modules/extra_mod.py
+++ MukeshRobot/modules/extra_mod.py
@@ -11,7 +11,7 @@ async def Mahadev(bot, message):
         response = requests.get(f'https://mukesh-api.vercel.app/mahadev') 
         x=response.json()["results"]
             
-        await message.reply_photo(photo=x,caption=f" \n๏ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➛ [๛ᴀ ᴠ ɪ s ʜ ᴀ ༗](https://t.me/avishaxbot) ", parse_mode=ParseMode.MARKDOWN)     
+        await message.reply_photo(photo=x,caption=f" \n๏ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➛ [˹ 𝗦𝙴𝙽𝙾𝚁𝙸𝚃𝙰 ✘ 𝗥𝙾𝙱𝙾 ˼​](https://t.me/StrangerSuperbot) ", parse_mode=ParseMode.MARKDOWN)     
     except Exception as e:
         await message.reply_text(f"**ᴇʀʀᴏʀ: {e} ")
 @Mukesh.on_message(filters.command("uselessfact"))
